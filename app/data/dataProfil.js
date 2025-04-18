96f8b0e30bb65c38888082b41079da2482cbd184
Guichon <guichon3@mmi-dev.unilim.fr>
2025-04-18 10:41:41 +0200

    it surprise

---
+++ app/data/dataProfil.js
@@ -0,0 +1,11 @@
+let HOST_URL = "https://guichon-sae203.mmi-limoges.fr/SAE2.03-Guichon-Alexandre";
+
+let DataProfil = {};
+
+DataProfil.readProfil = async function () {
+  let answer = await fetch(HOST_URL + "/server/script.php?todo=readProfil");
+  let profil = await answer.json();
+  return profil;
+};
+
+export { DataProfil };
